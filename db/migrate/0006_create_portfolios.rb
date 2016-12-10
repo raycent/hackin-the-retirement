a561334f156a18e41d14ffa92f109f24e5e146d1
Raycent <raycent.tan@flatironschool.com>
2016-12-10 13:49:13 -0500

    Add in portfolio details

--- db/migrate/0006_create_portfolios.rb
+++ db/migrate/0006_create_portfolios.rb
@@ -1,7 +1,14 @@
 class CreatePortfolios < ActiveRecord::Migration
   def change
     create_table :portfolios do |t|
-      t.belongs_to :asset
+      t.integer :us_stocks
+      t.integer :foreign_stocks
+      t.integer :emerging_market
+      t.integer :dividend_stocks
+      t.integer :corporate_bonds
+      t.integer :emerging_market_bonds
+      t.integer :real_estate
+      t.integer :commodity
     end
   end
 end
